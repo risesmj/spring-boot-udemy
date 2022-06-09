aa730292dc59377dc49e9ee7900ebfc28df33efa
MJ <matheusjcunha@gmail.com>
2022-06-09 15:12:39 -0300

    Initial commit

---
+++ carros/src/main/kotlin/com/example/carros/CarrosApplication.kt
@@ -0,0 +1,11 @@
+package com.example.carros
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarrosApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarrosApplication>(*args)
+}
